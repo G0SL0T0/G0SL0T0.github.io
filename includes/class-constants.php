468c8bdd865d9f851c797fb77959abe931184c6f
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-08-03 07:36:12 +0500

    # no backend

--- includes/class-constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-class WriteSonic_Analytics_Constants {
-    public const DEFAULT_INGESTION_DOMAIN = 'https://ingestion.writesonic.com';
-    public const API_ENDPOINT_ANALYTICS = 'api/v1/analytics/ingest';
-
-} 
