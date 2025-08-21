6cc1585b98bbade6fb0ced3cd64bdfc3cfb4363a
Gosloto <182990710+G0SL0T0@users.noreply.github.com>
2025-08-21 11:44:41 +0500

    --

--- src/types/achievement.ts
+++
@@ -1,9 +0,0 @@
-//src/types/achievement.ts
-export interface Achievement {
-  id: string;
-  name: string;
-  game: string;
-  date: string | null;
-  unlocked: boolean;
-  type: 'achievement' | 'challenge';
-}
--- src/types/game.ts
+++
@@ -1,9 +0,0 @@
-//src/types/game.ts
-export interface Game {
-  id: string;
-  name: string;
-  hours: number;
-  lastLaunch: string;
-  achievements: string;
-  image: string;
-}
